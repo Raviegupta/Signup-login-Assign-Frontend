5672147cda0df7e03efaf845cfd05586217fd0e9
Raviegupta <raviegupta153@gmail.com>
2024-02-14 17:09:20 +0530

    LoginSignUp Compo

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -4,6 +4,25 @@ import "./CSS/LoginSignUp.css";
 const LoginSignup = () => {
 
     const [state, setState] = useState('Login');
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    const changeHandler = (e) => {
+        setFormData({ ...formData, [e.target.name]:e.target.value })
+    }
+
+    const signup = async () => {
+        console.log('Sign Up fun is called');
+        console.log(formData);
+    }
+
+    const login = async () => {
+        console.log('login fun is called');
+        console.log(formData);
+    }
 
 
   return (
@@ -13,17 +32,17 @@ const LoginSignup = () => {
         <div className="loginsignup-fields">
             {
                 state==='Sign Up'
-                ? <input type="text" name="" id="" placeholder="Your name" />
+                ? <input type="text" name="username" value={formData.username} onChange={changeHandler} id="" placeholder="Your name" />
                 : <></>
             }
-          <input type="email" name="" id="" placeholder="Email Address" />
-          <input type="password" name="" id="" placeholder="Password" />
+          <input type="email" name="email" value={formData.email} onChange={changeHandler} id="" placeholder="Email Address" />
+          <input type="password" name="password" value={formData.password} onChange={changeHandler} id="" placeholder="Password" />
         </div>
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" required />
           <p>By continuing, i agree to the terms of use & privacy policy.</p>
         </div>
-        <button>Continue</button>
+        <button onClick={()=>{ state==='Sign Up'?signup():login() }}>Continue</button>
         {
             state==='Sign Up'
             ? <p className="loginsignup-login">Already have an account? <span onClick={()=>{setState('Login')}}>Login</span></p> 
